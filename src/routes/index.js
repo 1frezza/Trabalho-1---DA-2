a4357e9a5bf3b98d76218691069f88fbbfec69cf
Kauan Flávio Rosso <kauanrosso8@gmail.com>
2024-09-30 17:33:08 -0300

    Adiciona funcionalidades de controle de profissionais de saúde

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const studentsRoutes = require('./studentsRoutes')
+const profsaudeRoutes = require('./prof-saude')
 
 router.use(express.json())
-router.use('/students', studentsRoutes)
+router.use('/prof-saude', profsaudeRoutes)
 
 module.exports = router
